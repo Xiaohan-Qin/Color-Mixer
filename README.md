ed62b51960bc06b579c893237a354e92a4f87538
qinxhannah <qin.xia@northeastern.edu>
2022-01-28 18:31:25 -0800

    screenshots added

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Color-Mixer
+
+## Description
+This is an additive color mixing program that allows users to mix color based on the classical primary colors - red, green, and blue. Users can change the concentration of each color and the program would mix these color and blend it out on the background.
+
+## Prerequisite
+Need dependency: python turtle library. 
+You can do this by running ```pip install turtle```\
+(For python version 3 this package is pre-installed).
+
+## How to run
+Simply run ```python color_mixer.py```
+
+## How it works
+User's mouseevent changes the y-axis coordinates of the three primary colors. A fillcolor function would then take these three values as inputs and generates a corresponding background color.
+
+## A simple demo
+Here are some screenshots of the program output.
+Check out the video demo by downloading the video_demo.mp4 file. 
+
--- color_mixer.py
+++ color_mixer.py
@@ -1,34 +1,6 @@
+from ColorMixer import ColorMixer
 import turtle
 from turtle import Turtle, Screen
-
-
-class ColorMixer(Turtle):
-    def __init__(self, x, y):
-        Turtle.__init__(self)
-        self.shape("turtle")
-        self.resizemode("user")
-        self.shapesize(3, 3, 5)
-        self.pensize(7)
-        self._color = [0, 0, 0]
-        self.x = x
-        self._color[x] = y
-        r, g, b = self._color[0], self._color[1], self._color[2]
-        self.color(r, g, b)
-        self.speed(0)
-        self.left(90)
-        self.pu()
-        self.goto(x, 0)
-        self.pd()
-        self.sety(1)
-        self.pencolor("gray25")
-        self.ondrag(self.shift)
-
-    def shift(self, x, y):
-        self.sety(max(0, min(y, 1)))
-        self._color[self.x] = self.ycor()
-        r, g, b = self._color[0], self._color[1], self._color[2]
-        self.fillcolor(r, g, b)
-        set_bg_color()
 
 
 def set_bg_color():
@@ -58,3 +30,5 @@ if __name__ == "__main__":
     msg = main()
     print(msg)
     turtle.mainloop()
+
+    
